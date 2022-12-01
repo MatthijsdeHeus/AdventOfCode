b15e04d289ddebe9cd3027fb36719665a7eeebd8
Matthijs de Heus <38014878+MatthijsdeHeus@users.noreply.github.com>
2022-12-01 06:38:27 +0100

    Dag1

--- 2022/1.py
+++ 2022/1.py
@@ -1,3 +1,27 @@
 from Utilities import *
 
 data = GetData(2022, 1)
+
+def part1():
+    elfList = [0]
+
+    index = 0
+
+    for datapoint in data:
+        if len(datapoint) == 0:
+            elfList.append(0)
+            index += 1
+        else:
+            elfList[index] += int(datapoint)
+
+    print(max(elfList))
+
+    return elfList
+
+def part2():
+    elfList = part1()
+
+    elfList.sort(reverse=True)
+    print(elfList[0] + elfList[1] + elfList[2])
+
+part2()
